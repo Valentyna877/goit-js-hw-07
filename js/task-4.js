1d2a303e1d752d9b9aa0d747a23556d83b73c8e6
Valentyna <valentine177@gmail.com>
2025-10-28 20:48:27 +0200

    My seventh JS Homework

---
+++ js/task-4.js
@@ -0,0 +1,43 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const { email, password } = form.elements;
+
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (emailValue === "" || passwordValue === "") {
+        alert("All form fields must be filled in");
+        return;
+    }
+
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    };
+
+    console.log(formData);
+
+    form.reset();
+});
+
+
+
+const emailInput = form.elements.email;
+const passwordInput = form.elements.password;
+
+emailInput.addEventListener('focus', () => {
+    emailInput.placeholder = 'Type area';
+});
+emailInput.addEventListener('blur', () => {
+    emailInput.placeholder = '';
+});
+
+passwordInput.addEventListener('focus', () => {
+    passwordInput.placeholder = 'Type area';
+});
+passwordInput.addEventListener('blur', () => {
+    passwordInput.placeholder = '';
+});
