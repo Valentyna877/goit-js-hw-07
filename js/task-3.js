e34b7a3ffede194a81b04df193ea919ff9ee1d72
Valentyna <valentine177@gmail.com>
2025-11-01 16:55:36 +0200

    Created element div through the script in Task#3 for better design

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,14 @@
 const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
+const heading = output.closest("h1");
+
+const wrapper = document.createElement("div");
+
+wrapper.appendChild(input);
+wrapper.appendChild(heading);
+
+const reference = document.querySelector("p");
+reference.insertAdjacentElement("afterend", wrapper);
 
 input.addEventListener("input", () => {
     const trimmedValue = input.value.trim();
